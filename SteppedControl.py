c6e79d8204343bc60083f7f04cffe9e10d3d0377
KNChiu <y9760210@gmail.com>
2021-09-26 14:31:55 +0800

    開機自動馬達初始化、自動歸位歸零

--- SteppedControl.py
+++ SteppedControl.py
@@ -12,7 +12,7 @@ import io
 import threading
 
 class Stepped_API():
-    def __init__(self, serialPort, baudrate):
+    def __init__(self, serialPort, baudrate, toolmode=False):
         # serialPort="/dev/ttyTHS1",                       # Jetson nano TX,RX
         # serialPort="/dev/ttyUSB0",                       # Jetson nano USB
         # serialPort='COM20',                              # 電腦
@@ -21,8 +21,13 @@ class Stepped_API():
         self.offline = False
         self.motoIsmoving = True
         self.setSerial()
+        if toolmode==False:
+            self.setMoto()
+            time.sleep(0.1)
+            self.motoZeroset()
+            print("init Finish")
 
-    def setSerial(self):                            # 設定連線通道
+    def setSerial(self):                                # 設定連線通道
         self.serial_port = serial.Serial(
             port=self.serialPort,            
             baudrate=self.baudrate,
@@ -34,8 +39,7 @@ class Stepped_API():
         )
         time.sleep(0.1)
 
-
-    def reconnect(self):                            # Serial port 重新連線
+    def reconnect(self):                                # Serial port 重新連線
         self.serial_port.close()                    # 關閉 Serial port
         self.offline = True                         # 斷線旗標
         while self.offline:                         # 檢測是否回復
@@ -47,8 +51,7 @@ class Stepped_API():
                 self.offline = True                 # 斷線旗標
                 continue
     
-
-    def readSerial(self, strChar=True):             # 監視序列
+    def readSerial(self, strChar=True):                 # 監視序列
         serialData = ""                             # 讀取到的資料
         timeOutcheck = False                        # timeout 檢查旗標
         timestart = 0                               # timeout 計時
@@ -90,32 +93,29 @@ class Stepped_API():
 
         return serialData
 
-
-    def sendSerial(self, sendData, willreturn=True):
-        time.sleep(0.01)
+    def sendSerial(self, sendData, willreturn=True):    # 傳送序列副程式
+        time.sleep(0.05)
         try:
             sendData = sendData + '\r\n'                                # 加入輸入與換行符號
+            # print(sendData.encode('utf-8'))
             self.serial_port.write(sendData.encode('utf-8')) 
-            time.sleep(0.001)
-
+            time.sleep(0.05)
             if willreturn:                                              # 是否回傳狀態
                 if sendData == 'r\r\n':                                 # 由於r(及時告知目前平台是否有移動)指令僅回傳1位元
                     returnCMD = self.readSerial(strChar=False)          # 讀取回應(字元模式)
                 else:
                     returnCMD = self.readSerial(strChar=True)           # 讀取回應(字串模式)
-
                 return returnCMD
         except Exception as e:
             print("Send erro", e)
-            self.reconnect()                                    # 重啟 Serial port
+            self.reconnect()                                            # 重啟 Serial port
 
-
-    def checkMoto(self):
-        self.motoIsmoving = True
+    def checkMoto(self):                                                # 確認馬達狀態
+        self.motoIsmoving = True                                        # 馬達狀態旗標
         while self.motoIsmoving:
             time.sleep(0.1)
             check = self.sendSerial('r', willreturn=True)
-            print(check)
+            start = time.time()
             if check == 'L':
                 self.motoIsmoving = False
             elif check == '.':
@@ -123,30 +123,26 @@ class Stepped_API():
             else:
                 print("check moto is erro!")
                 return False
+        time.sleep(0.01)
         
-
-    def setMoto(self):
+    def setMoto(self):                                                  # 設定馬達初始化
         self.sendSerial(":SI2000,4000,1000", willreturn=False)
 
+    def motoAbsolute(self, lift, rotated):                              # 絕對座標控制(:P1000,1000,0)
+        motoCMD = ":P" + str(lift) + "," + str(rotated) + ",0"         
+        self.sendSerial(motoCMD, willreturn=False) 
+        self.checkMoto()
 
-    def motoAbsolute(self, lift, rotated):
-        motoCMD = ":P" + str(lift) + "," + str(rotated) + ",0"          # 絕對座標控制(:P1000,1000,0)
-        self.sendSerial(motoCMD, willreturn=False)             
-        # return_X, rotatedGET, liftGET = motoGET.split('\t',2)           # 以"\t"隔開
-        # return rotatedGET, liftGET
-    
+    def motoRelative (self, lift, rotated):                             # 相對座標控制(:F1000,1000,0)
+        motoCMD = ":F" + str(lift) + "," + str(rotated) + ",0"          
+        self.sendSerial(motoCMD, willreturn=False)   
+        self.checkMoto()
+        
 
-    def motoRelative (self, lift, rotated):
-        motoCMD = ":F" + str(lift) + "," + str(rotated) + ",0"          # 相對座標控制(:F1000,1000,0)
-        self.sendSerial(motoCMD, willreturn=False)                      
-        # return_X, rotatedGET, liftGET = motoGET.split('\t',2)           # 以"\t"隔開
-        # return rotatedGET, liftGET
-
-
-    def motoZeroset(self):
-        self.motoRelative(1000000,-50000)
-
-        print(self.motoIsmoving)
+    def motoZeroset(self):                                              # 座標歸零
+        time.sleep(1)
+        self.motoRelative(100000, 0)                                    # 相對座標控制(強制移動到極限位置)
+        self.motoRelative(0, -50000)                                    # 相對座標控制(強制移動到極限位置)
 
         self.checkMoto()
         while self.motoIsmoving:
@@ -158,18 +154,12 @@ class Stepped_API():
 
 
 if __name__ == '__main__':
-    Stepped = Stepped_API(serialPort='COM2', baudrate=9600)
-    # rotatedGET, liftGET = Stepped.motoAbsolute(-1000, 500)
-    # print("移動後絕對座標 :", rotatedGET, liftGET)
-
-    # returnCMD = Stepped.sendSerial(':F1000,-500,0')
+    Stepped = Stepped_API(serialPort='COM2', baudrate=9600, toolmode=False)  # 初始化(COM, baudrate, toolmode)
+    print("--------------------")
+    Stepped.motoRelative(-10000, 5000)                                      # 相對座標控制
     returnCMD = Stepped.sendSerial(':RP')
     print(returnCMD)
 
-    Stepped.motoZeroset()
-
-    returnCMD = Stepped.sendSerial(':RP')
-    print(returnCMD)
 
 
 
